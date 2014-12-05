51703d8e3b95580555e7bea36222aa9a5f06eb5c
Liu-Luke <405653510@qq.com>
2014-12-05 11:29:22 +0800

    UDP日志服务器基本结构搭建完成，并可以直接打包使用，后期将继续完善

---
+++ src/main/java/org/server/log/mina/handler/DispatchHandler.java
@@ -0,0 +1,37 @@
+package org.server.log.mina.handler;
+
+import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DispatchHandler extends IoHandlerAdapter {
+	private final static Logger LOGGER = LoggerFactory.getLogger(DispatchHandler.class);
+
+	@Override
+	public void sessionCreated(IoSession session) {
+		LOGGER.debug("创建连接,客户端地址:"+session.getRemoteAddress());
+	}
+
+	@Override
+	public void sessionOpened(IoSession session) throws Exception {
+		LOGGER.debug("打开连接");
+	}
+
+	@Override
+	public void messageReceived(IoSession session, Object message) {
+		LOGGER.debug("收到数据:"+(String)message);
+	}
+
+	@Override
+	public void sessionClosed(IoSession session) throws Exception {
+		
+		LOGGER.debug("关闭连接");
+	}
+
+	@Override
+	public void exceptionCaught(IoSession session, Throwable cause) {
+		LOGGER.error("通讯异常:", cause);
+	}
+
+}
